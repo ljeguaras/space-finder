7884402ef8c196165484c131e049e752d9067fa1
Lester John Eguaras <lesterjohneguaras@outlook.com>
2025-07-22 16:21:00 +0800

    adopt NodejsFunction for simplified build process, tree-shaking, and reduced Lambda size

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,17 +1,26 @@
 import {Stack, StackProps} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import {Code, Function as LambdaFunction, Runtime} from 'aws-cdk-lib/aws-lambda';
+import {Code, Runtime} from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import { ITable } from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+
+interface LambdaStackProps extends StackProps{
+    spacesTable : ITable
+}
 export class LambdaStack extends Stack{
     public readonly helloLambdaIntegration: LambdaIntegration
-    constructor(scope:Construct, id: string,props?:StackProps) {
+    constructor(scope:Construct, id: string,props?:LambdaStackProps) {
         super(scope,id,props);
         
-        const helloLambda = new LambdaFunction(this,'HelloLambda',{
+        const helloLambda = new NodejsFunction(this,'HelloLambda',{
             runtime: Runtime.NODEJS_18_X,
-            handler: 'hello.main',
-            code:Code.fromAsset(join(__dirname,'..','..','services'))
+            handler: 'handler',
+            entry:join(__dirname,'..','..','services','hello.ts'),
+            environment:{
+                TABLE_NAME: props.spacesTable.tableName
+            }
         });
         this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
     }
--- src/services/hello.js
+++
@@ -1,3 +0,0 @@
-exports.main = async  function (event,context){
-    return {statusCode:200,body:JSON.stringify("hello from lambda!")}
-}
